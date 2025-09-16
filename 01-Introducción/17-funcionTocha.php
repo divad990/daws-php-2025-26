289275082764d5b26ca73a94eca92a7168f05f01
David Gonzalez <david.gonzalez.48848@iksale.egibide.org>
2025-09-17 00:11:20 +0200

    Ejercicio 17

---
+++ 01-Introducción/17-funcionTocha.php
@@ -0,0 +1,21 @@
+<?php
+
+    function validar ($user,$passwd) {
+
+        if ($usuarios[$user][$passwd])
+
+        $usuarios = [
+            "user1" => [
+                "nombre" => "Nora",
+                "password" => "123123",
+                "email" => "nora@php.net"
+            ],
+            "user2" => [
+                "nombre" => "David",
+                "password" => "Jm12345",
+                "email" => "div.divad990@gmail.com"
+            ],
+        ];
+    };
+
+?>
