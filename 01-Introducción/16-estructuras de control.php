233cbd5c36d263fc430378bf477b6976b41b0f9f
David Gonzalez <david.gonzalez@ikasle.egibide.org>
2025-09-16 09:29:14 +0200

    Ejercicio 16

---
+++ 01-Introducción/16-estructuras de control.php
@@ -0,0 +1,16 @@
+<?php
+
+$num1 = $_GET['num1'];
+$num2 = $_GET['num2'];
+
+echo "El resultado de la operación es: " . operacion($num1, $num2);
+
+function operacion ($num1, $num2) {
+    if ($num1 != $num2) {
+        return $num1 + $num2;
+    } else {
+        return $num1 * $num2;
+    };
+};
+
+?>
